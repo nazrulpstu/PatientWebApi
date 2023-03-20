63600abe8b1bd715549b3cb7f05e925bdf09c63a
nazrulca7 <nazrulca7@gmail.com>
2023-03-20 00:09:33 -0400

    done

--- Interfaces/IAuthenticate.cs
+++ Interfaces/IAuthenticate.cs
@@ -7,14 +7,6 @@ namespace PatientWebApi.Interfaces
 
         Task<IEnumerable<Users>> getuser();
         Task<(bool IsSuccess, Users user, String ErrorMessage)> AuthenticateUser(string username, string passcode);
-        //Task<(bool IsSuccess, IEnumerable<Users> user, String ErrorMessage)> VerifyPasswordHash(object request);
-        //Task<(bool IsSuccess, int Id, String ErrorMessage)> Add(Patients obj);
-        //// public bool Edit(int id, Patients obj);
-        //Task<(bool IsSuccess, int Id, String ErrorMessage)> UpdateAsync(int Id, Patients obj);
-        ////public bool DeletePatent(int id);
-        //Task<(bool IsSuccess, String ErrorMessage)> DeleteAsync(int Id);
-
-        //Task<(bool IsSuccess, IEnumerable<Patients> Patients, String ErrorMessage)> GetPatientByIdAsync(int patientId);
-
+      
     }
 }
